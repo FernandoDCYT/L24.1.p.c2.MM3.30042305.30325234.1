24ce3d2721180253d9cb341823fa5b427c9b3372
FernandoDCYT <1001.30325234.ucla@gmail.com>
2024-10-10 10:34:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-import Cl_fijo from "./Cl_fijo.js";
+import Cl_becado from "./Cl_becado.js"
 
-let f1 = new Cl_fijo("Carlos", 25, 100);
-let f2 = new Cl_fijo("Carolina", 40, 90);
+let beca1 = new Cl_becado("Mary", 132, 8);
+let beca2 = new Cl_becado("Alirio", 315, 35);
 
-let ingresoF1 = f1.calcIngreso();
-let ingresoF2 = f2.calcIngreso();
+beca1.calcBeca();
+beca2.calcBeca();
 
 let salida = document.getElementById("salida");
 
 salida.innerHTML = `
-    Nombre del profesor fijo: ${f1.nombre}<br>
-    Monto del bono: $${f1.bono}<br>
-    Monto del sueldo: $${f1.sueldo}<br>
-    Ingreso total del profesor ${f1.nombre}: $${ingresoF1}<br><br>
-    
-    Nombre del profesor fijo: ${f2.nombre}<br>
-    Monto del bono: $${f2.bono}<br>
-    Monto del sueldo: $${f2.sueldo}<br>
-    Ingreso total del profesor ${f2.nombre}: $${ingresoF2}<br>
+    Nombre del estudiante: ${beca1.nombre}<br>
+    Acumulado en notas: ${beca1.notAc}<br>
+    Cantidad de materias: ${beca1.mat}<br>
+    Nota promedio: ${beca1.calcProm().toFixed(2)}<br>
+    ${beca1.calcBeca()}
+    <br><br>
+    Nombre del estudiante: ${beca2.nombre}<br>
+    Acumulado en notas: ${beca2.notAc}<br>
+    Cantidad de materias: ${beca2.mat}<br>
+    Nota promedio: ${beca2.calcProm().toFixed(2)}<br>
+    ${beca2.calcBeca()}
 `;
